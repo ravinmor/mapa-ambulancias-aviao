7b35b908b87a4b781769f444c91d7442dc380550
Ravin Mor <ravinmenezes@outlook.com>
2026-09-11 16:00:39 -0300

    Revert "Renova frota de aeronaves rastreadas nos scripts start.bat da parede (sem Docker)"
    This reverts commit c7c90f35df4f699d220a24a8253bdca2bb1860ac.

--- api/start.bat
+++ api/start.bat
@@ -10,11 +10,5 @@ set BROADCAST_INTERVAL_MS=5000
 set AIRCRAFT_BROADCAST_INTERVAL_MS=30000
 set CORS_ORIGIN=*
 
-rem MESMA variavel/valor do sync-job/start.bat (ver comentario la) -- precisa
-rem ser identica pra api filtrar a listagem (getTrackedAircraft) pra frota
-rem configurada agora, em vez de devolver toda linha historica (bug
-rem corrigido 2026-09-09: "ainda tem varias aeronaves na frota").
-set TRACKED_AIRCRAFT_ICAO24S=e48006,e49e3a,e48987,e49bfd
-
 cd /d "%~dp0"
 "%NODE_EXE%" dist\index.js
